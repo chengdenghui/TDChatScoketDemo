9f77a632bbdc098b2443c7dc2c3dc163821a3ce5
denghui <2037276628@qq.com>
2018-03-18 11:10:35 +0800

    首次提交
    首次提交

---
+++ TDChatScoketDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TDChatScoketDemo
+//
+//  Created by mac on 2018/3/18.
+//  Copyright © 2018年 hui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TDChatScoketDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TDChatScoketDemo
+//
+//  Created by mac on 2018/3/18.
+//  Copyright © 2018年 hui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
