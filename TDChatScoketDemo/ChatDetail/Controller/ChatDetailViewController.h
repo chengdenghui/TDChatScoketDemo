5b1290b9bc501cecc69de2ddce645cf5e24169e8
denghui <2037276628@qq.com>
2018-03-18 11:28:38 +0800

    1
    1

---
+++ TDChatScoketDemo/ChatDetail/Controller/ChatDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChatDetailViewController.h
+//  TDChatScoketDemo
+//
+//  Created by mac on 2018/3/18.
+//  Copyright © 2018年 hui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChatDetailViewController : UIViewController
+
+@end
---
+++ TDChatScoketDemo/ChatDetail/Controller/ChatDetailViewController.m
@@ -0,0 +1,37 @@
+//
+//  ChatDetailViewController.m
+//  TDChatScoketDemo
+//
+//  Created by mac on 2018/3/18.
+//  Copyright © 2018年 hui. All rights reserved.
+//
+
+#import "ChatDetailViewController.h"
+
+@interface ChatDetailViewController ()
+
+@end
+
+@implementation ChatDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
