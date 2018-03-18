3cca6cfc75fbd066833c0a05fd908467e4ffe7d9
chengdenghui <cheng_deng_hui@163.com>
2018-03-18 22:33:42 +0800

    4
    4

---
+++ TDChatScoketDemo/MainChatList/Model/ChatListModel.h
@@ -0,0 +1,13 @@
+//
+//  ChatListModel.h
+//  TDChatScoketDemo
+//
+//  Created by 程登伟 on 2018/3/18.
+//  Copyright © 2018年 hui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ChatListModel : NSObject
+
+@end
---
+++ TDChatScoketDemo/MainChatList/Model/ChatListModel.m
@@ -0,0 +1,13 @@
+//
+//  ChatListModel.m
+//  TDChatScoketDemo
+//
+//  Created by 程登伟 on 2018/3/18.
+//  Copyright © 2018年 hui. All rights reserved.
+//
+
+#import "ChatListModel.h"
+
+@implementation ChatListModel
+
+@end
